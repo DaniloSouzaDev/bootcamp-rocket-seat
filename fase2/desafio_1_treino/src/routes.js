7bb0c1c3ac59992cfc8e417a073c40704d0a364a
DaniloSouzaDev <gtd.danilosouza@gmail.com>
2019-10-02 16:47:53 -0300

    Configurações do Webpack, Babe e Css

--- fase2/desafio_1_treino/src/routes.js
+++ fase2/desafio_1_treino/src/routes.js
@@ -1,3 +1,64 @@
+import Router from "express";
+
+const routes = new Router();
+
+const projects = [];
+
+routes.post("/projects", (req, res) => {
+  const {id, title} = req.body;
+  const project = {
+    id,
+    title,
+    task = []
+  };
+  projects.push(project);
+  return res.json(project);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import { Router } from "express";
 const routes = Router();
 
